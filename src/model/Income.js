89e4108c2bae627a51efc022889d9c4fdd000d69
Jefferson Pedro <jeffersonpedro89@gmail.com>
2022-09-01 20:52:56 -0300

    remove on model the category field

--- src/model/Income.js
+++ src/model/Income.js
@@ -4,7 +4,6 @@ const {Schema} = mongoose;
 const incomeSchema = new Schema({
     id: { type: String },
     description: { type: String, required: true },
-    category: { type: String },
     value: { type: Number, required: true },
     date: { type: Date, required: true }
 });
