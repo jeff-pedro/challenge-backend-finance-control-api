daca6d2d57ae903ff8d4b0bb03cda31d199dc2e4
Jefferson Pedro <jeffersonpedro89@gmail.com>
2022-09-05 22:50:59 -0300

    add expense and income summary: * new endpoint summary/{year}/{month} * summary with expense and income total amount, balance, and total   expenses amount by category

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import incomes from "./incomesRoutes.js";
 import expenses from "./expensesRoutes.js";
+import summary from "./summaryRoutes.js";
 
 const routes = (app) => {
     app.route('/').get((req, res) => {
@@ -10,7 +11,8 @@ const routes = (app) => {
     app.use(
         express.json(),
         incomes,
-        expenses
+        expenses,
+        summary
     );
 }
 
