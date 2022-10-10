7ce437fa25c6331921ed9e932cd18e13b1aee7d9
Jefferson Pedro <jeffersonpedro89@gmail.com>
2022-10-10 17:39:15 -0300

    add morgan middleware to handle request logs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,20 @@
-import express from "express";
-import db from "./config/dbConnect.js";
-import routes from "./routes/index.js";
-
-// database's connection
-db.on("error", console.log.bind(console, "Connection error - "));
-db.once("open", () => {
-    console.log("Database connected!");
-});
+import express from 'express';
+import logger from 'morgan';
+import db from './config/dbConnect.js';
+import routes from './routes/index.js';
 
 const app = express();
+
+// database's connection
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// use morgan to log at command line
+app.use(logger('combined', {
+  // don't show the log when it is test
+  skip: (req, res) => process.env.NODE_ENV === 'test',
+}));
+
+// call the routes
 routes(app);
 
 export default app;
