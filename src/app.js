3f5a58c69b8720b6a65d191b1525aa52fb8579db
Jefferson Pedro <jeffersonpedro89@gmail.com>
2022-11-22 22:23:01 -0300

    imports body-parser and passport to app

--- src/app.js
+++ src/app.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import logger from 'morgan';
+import bodyParser from 'body-parser';
 import db from './config/db.js';
 import routes from './routes/index.js';
+
+// passport strategies to authenticate
+import './auth/strategies.js';
 
 const app = express();
 
 // database's connection
 db.on('error', console.error.bind(console, 'connection error:'));
 
-// use morgan to log at command line
+// use morgan to log requests
 app.use(logger('combined', {
   // don't show the log when it is test
   skip: (req, res) => process.env.NODE_ENV === 'test',
 }));
+
+// use body-parser to transform json to object
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 routes(app);
 
