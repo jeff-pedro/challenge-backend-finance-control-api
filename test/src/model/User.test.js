7cd6911376d26906cbbf4f9eb691e70ab0b6c7d7
Jefferson Pedro <jeffersonpedro89@gmail.com>
2022-11-22 22:36:29 -0300

    remove password field from tests

--- test/src/model/User.test.js
+++ test/src/model/User.test.js
@@ -6,24 +6,22 @@ describe('Users', () => {
   });
 
   describe('Creating documents', () => {
-    it('should creates an user', (done) => {
+    it('should creates a user', (done) => {
       const newUser = new Users({
         username: 'admin',
         email: 'admin@admin.com',
-        password: '1234567',
       });
 
       newUser.save().then((user) => {
         user.should.be.an('object');
         user.should.have.property('username');
         user.should.have.property('email');
-        user.should.have.property('password');
         user.should.have.property('_id');
         done();
       });
     });
 
-    it('should not creates an user without an username', (done) => {
+    it('should not creates a user without an username', (done) => {
       const newUser = new Users({
         email: 'admin@admin.com',
         password: '123',
@@ -35,7 +33,7 @@ describe('Users', () => {
       });
     });
 
-    it('should not creates an user without an email', (done) => {
+    it('should not creates a user without an email', (done) => {
       const newUser = new Users({
         username: 'admin',
         password: '123',
@@ -47,19 +45,7 @@ describe('Users', () => {
       });
     });
 
-    it('should not creates an user without an password', (done) => {
-      const newUser = new Users({
-        username: 'admin',
-        email: 'admin@admin.com',
-      });
-
-      newUser.save((err) => {
-        if (err) return done();
-        throw new Error('Should generate error');
-      });
-    });
-
-    it('should not creates with incorrect property format', (done) => {
+    it('should not creates a user with incorrect property format', (done) => {
       const newUser = new Users({
         wrongUserName: 'wrong property',
         email: 'wrong@wrong.com',
@@ -71,25 +57,13 @@ describe('Users', () => {
         throw new Error('Should generate error');
       });
     });
-
-    it('should not creates when given an password less than 7 chars', (done) => {
-      const newUser = new Users({
-        username: 'elanor',
-        email: 'elanor@harfoots.com',
-        password: 'nori', // should be at least 7 chars long
-      });
-      newUser.save((err) => {
-        if (err) return done();
-        throw new Error('Should generete error');
-      });
-    });
   });
 
   describe('Findind documents', () => {
     it('should retrieve all users', (done) => {
-      Users.find({}, (err, user) => {
-        user.should.be.an('array');
-        user.length.should.be.eql(1);
+      Users.find({}, (err, users) => {
+        users.should.be.an('array');
+        users.length.should.be.eql(1);
         done();
       });
     });
@@ -104,20 +78,20 @@ describe('Users', () => {
   });
 
   describe('Updating documents', () => {
-    it('should update an user given its id', (done) => {
+    it('should update a user given its id', (done) => {
       const conditions = { username: 'admin' };
-      const update = { password: '321' };
+      const update = { email: 'admin2@admin.com' };
       const options = { new: true };
       Users.findOneAndUpdate(conditions, update, options, (err, user) => {
         user.should.be.an('object');
-        user.should.have.property('password').eql('321');
+        user.should.have.property('email').eql('admin2@admin.com');
         done();
       });
     });
   });
 
   describe('Deleting documents', () => {
-    it('should delete an user', (done) => {
+    it('should delete a user', (done) => {
       const conditions = { username: 'admin' };
       Users.deleteOne(conditions, (err, result) => {
         result.should.be.an('object');
